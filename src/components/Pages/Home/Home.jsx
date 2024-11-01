4eb3dab5294e5f7af7a4db0a6a84e0a466fcd70e
SENA <SENA@ARMSCCCCTGOD079.FormacionSena.RED>
2024-11-01 11:19:40 -0500

    Segundo commt de pruebas con intru

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {Header} from '../../layouts/Header/Header'
 import Section from '../../layouts/Section/Section'
 import { Section2 } from '../../layouts/SectionProyects/Section2'
+import { Prueba } from '../../layouts/Prueba/Prueba'
 import '../../../index.css'
+
 
 
 
@@ -12,6 +14,7 @@ export const Home = () => {
       <Header/>
       <Section/>
       <Section2/>
+      <Prueba/>
     </>
   )
 }
